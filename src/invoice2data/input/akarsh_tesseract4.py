808c2065aba1a261f2dfccd75840e452cc3c1ac6
Akarsh Saxena <41925598+akarsh-saxena@users.noreply.github.com>
2019-09-11 19:41:02 +0530

    Update akarsh_tesseract4.py

--- src/invoice2data/input/akarsh_tesseract4.py
+++ src/invoice2data/input/akarsh_tesseract4.py
@@ -2,6 +2,5 @@ import pytesseract
 from PIL import Image
 
 def to_text(path, lang='eng'):
-	pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 	text = pytesseract.image_to_string(Image.open(path), lang=lang, config='--oem 1 --psm 3')
 	return text
