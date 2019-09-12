aec2c37ea17417a71a85f5be5e6a5c9916252007
Akarsh Saxena <41925598+akarsh-saxena@users.noreply.github.com>
2019-09-12 14:46:22 +0530

    Update akarsh_tesseract4.py

--- src/invoice2data/input/akarsh_tesseract4.py
+++ src/invoice2data/input/akarsh_tesseract4.py
@@ -1,6 +1,6 @@
 import pytesseract
 from PIL import Image
 
-def to_text(path, lang='eng'):
-	text = pytesseract.image_to_string(Image.open(path), lang=lang, config='--oem 1 --psm 4')
+def to_text(path, lang='eng', oem='1', psm='3'):
+	text = pytesseract.image_to_string(Image.open(path), lang=lang, config='--oem '+oem+' --psm '+psm)
 	return text
